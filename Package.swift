a5f949dcfb03d630fb77566bb4ff3e92a5ca9073
Shubham Garg <91garg.shubham@gmail.com>
2023-08-09 11:37:39 +0530

    add PayU Analytics

--- Package.swift
+++ Package.swift
@@ -2,6 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+let VERSION_ANALYTICS_KIT: PackageDescription.Version = "3.0.0"
 
 let package = Package(
     name: "PayUIndia-CommonUI",
@@ -10,9 +11,20 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PayUIndia-CommonUI",
-            targets: ["PayUCommonUI"]),
+            targets: ["PayUIndia-CommonUITarget"])
     ],
+    dependencies: [
+        .package(name: "PayUIndia-Analytics", url: "https://github.com/payu-intrepos/PayUAnalytics-iOS.git", from: VERSION_ANALYTICS_KIT)
+        ],
     targets: [
-        .binaryTarget(name: "PayUCommonUI", path: "./PayUCommonUI.xcframework")
+        .binaryTarget(name: "PayUCommonUI", path: "./PayUCommonUI.xcframework"),
+        .target(
+            name: "PayUIndia-CommonUITarget",
+            dependencies: [
+                .product(name: "PayUIndia-Analytics", package: "PayUIndia-Analytics"),
+                "PayUCommonUI"
+            ],
+            path: "Wrappers/PayUIndia-CommonUIWrapper"
+        ),
     ]
 )
